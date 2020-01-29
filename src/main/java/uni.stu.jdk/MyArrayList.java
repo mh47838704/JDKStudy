ef90a51522d07d40564c0e48d5a6cdb186f99b41
嘟嘟龙 <850867443@qq.com>
2020-01-29 18:23:45 +0800

    添加了部分注释，完善了capacity的判断

--- src/main/java/uni.stu.jdk/MyArrayList.java
+++ src/main/java/uni.stu.jdk/MyArrayList.java
@@ -1,9 +1,11 @@
 package uni.stu.jdk;
 
+/*
+ **简单的ArrayList，实现add()方法
+ */
 public class MyArrayList<E> {
 
-
-    private static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final Object[] EMPTY_ELEMENTDATA = {};//定义空的数组
     //定义数组
     private Object[] elementData;
     //定义容量
@@ -21,47 +23,43 @@ public class MyArrayList<E> {
     /**
      * 定义有参构造函数
      *
-     * @param capacity//容量大小
+     * @param capacity 容量大小
      */
     public MyArrayList(int capacity) {
         if (capacity > 0) {
             elementData = new Object[capacity];
         } else if (capacity == 0) {
             elementData = EMPTY_ELEMENTDATA;
-        } else return;
+        } else throw new IllegalArgumentException("Illegal Capacity: " +
+                capacity);
     }
 
     /**
      * add()方法
      *
-     * @param element
+     * @param element 定义元素
      */
     public void add(E element) {
         if (size >= elementData.length) {
             return;
         }
         elementData[size++] = element;
-
     }
 
     /**
      * 重写toString()方法
      */
     public String toString() {
-
         StringBuilder sb = new StringBuilder();
-
         sb.append("[");
         for (int i = 0; i < size; i++) {
             sb.append(elementData[i] + ",");
         }
         sb.setCharAt(sb.length() - 1, ']');  //替换最后一个,为]
-
         return sb.toString();
     }
 
-
-    // 测试
+    // 测试用例
     public static void main(String[] args) {
         MyArrayList s1 = new MyArrayList(10);
         s1.add("aa");
@@ -77,7 +75,6 @@ public class MyArrayList<E> {
         s1.add("aa");
         s1.add("aa");
         System.out.println(s1);
-
     }
 }
 
